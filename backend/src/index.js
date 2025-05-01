6a99db543a8b003df6f76cb322a9e308823b01ce
Aaus <80692936+manameaaus@users.noreply.github.com>
2025-05-02 04:44:20 +0530

    changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,10 @@ import cors from "cors";
 
 import path from "path";
 
+import { connectDB } from "./lib/db.js";
+
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
-
-import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
@@ -30,12 +30,13 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 server.listen(PORT, () => {
-  console.log(`server is running on PORT:${PORT}`);
+  console.log("server is running on PORT:" + PORT);
   connectDB();
 });
